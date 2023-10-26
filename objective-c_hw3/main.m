ef33e739af9b11ac089004c02c2ca708ed1492dd
dsail <dsail@yandex.ru>
2023-10-26 09:13:17 +0300

    Initial Commit

---
+++ objective-c_hw3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objective-c_hw3
+//
+//  Created by Eugene Zvyagin on 26.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
